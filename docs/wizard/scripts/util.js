3f8c40a9b44ec16ace6a0dcdfd5aa5fc0e5877f2
vinit <vinit@observe.ai>
2019-06-07 15:09:26 +0530

    Changes for genesys demo

--- docs/wizard/scripts/util.js
+++ docs/wizard/scripts/util.js
@@ -1,25 +1,23 @@
 var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split("&"),
+    sParameterName,
+    i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split("=");
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
     }
+  }
 };
 
 /**
  * Switches to a different page and attaches the language as query param
  * This could also be stored in a cookie/session instead
- * @param {Sring} page 
+ * @param {Sring} page
  */
-var goToPage = function goToPage(page){
-    window.location = getUrlParameter('langTag') ? 
-        page + '.html?langTag=' + getUrlParameter('langTag') : 
-        page + '.html?langTag=en-us';
+var goToPage = function goToPage(page) {
+  window.location = page + ".html";
 };
--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 /*
-* Copyright (c) 2015 Interactive Intelligence
-*
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
+ * Copyright (c) 2015 Interactive Intelligence
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 var http = require("http");
-var https = require("https"),
-    fs = require("fs");
-var express = require('express');
+var express = require("express");
 var app = express();
-var cors = require('cors');
+var cors = require("cors");
 
-app.use(express.static(__dirname+ "/docs"));
+app.use(express.static(__dirname + "/docs"));
 app.use(cors());
-app.options('*', cors());
-app.set('port', process.env.PORT || 3000);
-
-var sslOptions = {
-    key: fs.readFileSync('https-requirements/_localhost.key'),
-    cert: fs.readFileSync('https-requirements/_localhost.crt'),
-    requestCert: true,
-    rejectUnauthorized: false
-};
+app.options("*", cors());
+app.set("port", process.env.PORT || 3000);
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(sslOptions, app);
 
-var httpsPort = 3001;
-
-httpServer.listen(app.get('port'));
-console.log("App started on port : " + app.get('port'));
-
-// console.log("starting on " + httpsPort + ' (https)');
-// httpsServer.listen(httpsPort);
+httpServer.listen(app.get("port"));
+console.log("App started on port : " + app.get("port"));
