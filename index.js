d3f0f8d8092bace62c3c08d25345cc3f4f2771fc
vinit <vinit@observe.ai>
2019-06-07 15:44:16 +0530

    Allow cors options for iframe

--- index.js
+++ index.js
@@ -11,8 +11,25 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
 app.use(express.static(__dirname + "/docs"));
 app.use(cors());
+app.use(allowCrossDomain);
 app.options("*", cors());
 app.set("port", process.env.PORT || 3000);
 
